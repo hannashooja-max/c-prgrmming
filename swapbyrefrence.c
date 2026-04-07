d039bf3d3394e0fcd818e76f19b8f0abcee6752c
Hanna Shooja <hannashooja@ug.cusat.ac.in>
2026-04-07 05:31:41 +0530

    Create swapbyrefrence.c

---
+++ swapbyrefrence.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int *x,int *y)
+{
+    int z;
+    z=*x;
+    *x=*y;
+    *y=z;
+    printf("Values after swapping x=%d,y=%d\n",*x,*y);
+}
+int main()
+{
+    int x,y;
+    printf("enter values for x and y: ");
+    scanf("%d %d",&x,&y);
+    swap(&x,&y);
+    printf("Values of x=%d,y=%d",x,y);
+    return 0;
+}
